fded142c6233eb571f3ce6eee533fea87f4dc82b
codedeki <serbian_5@hotmail.com>
2020-01-17 22:45:41 -0500

    added events post-type

--- page.php
+++ page.php
@@ -20,9 +20,9 @@ while(have_posts()) {
         $theParentId = wp_get_post_parent_id(get_the_ID());
         if($theParentId) { ?>    
     <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentId);?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParentId); ?></a> <span class="metabox__main"><?php the_title();?></span></p> 
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentId); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParentId); ?></a> <span class="metabox__main"><?php the_title();?></span></p> 
     </div>
-     <?   }
+     <?php   }
     ?>
 
     <?php 
@@ -32,9 +32,9 @@ while(have_posts()) {
     
     // <!-- adds permalinks for pages that do have a parent or child -->
     // if false returns nothing, removing the permalinks from pages without parent or child
-    if($theParentId or $testArray) { ?>
+    if ($theParentId or $testArray) { ?>
     <div class="page-links">
-      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParentId);?>"><?php echo get_the_title($theParentId);?></a></h2>
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParentId);?>"><?php echo get_the_title($theParentId); ?></a></h2>
       <ul class="min-list">
           <?php 
             if($theParentId) {
@@ -61,8 +61,7 @@ while(have_posts()) {
 
   </div>
 
-<?php
-}
+<?php }
 
 get_footer();
 
